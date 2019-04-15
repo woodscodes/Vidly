2593676a2e5e940c1a9a35946767a39189dc61db
stuwo <stooberto@gmail.com>
2019-04-15 11:00:59 +0900

    Added membership type model and annotations to customer model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; } // Nav property - from Cx to Membership type
+        public byte MembershipTypeId { get; set; } // Foreign key to Membership type (suffixed by Id - EF conventions)
     }
 }
