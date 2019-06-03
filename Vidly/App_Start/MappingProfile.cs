f93f2bf7f7db304eed48af3e1ffa8105b01491b7
stuwo <stooberto@gmail.com>
2019-06-03 12:24:15 +0900

    Added Web api for movies

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDTO>();
+            Mapper.CreateMap<CustomerDTO, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+
+            Mapper.CreateMap<Movie, MovieDTO>();
+            Mapper.CreateMap<MovieDTO, Movie>()
+                .ForMember(m => m.Id, opt => opt.Ignore());
+        }
+    }
+}
---
+++ Vidly/DTOs/CustomerDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        //[Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        public byte MembershipTypeId { get; set; } // Foreign key to Membership type (suffixed by Id - EF conventions)
+    }
+}
